caf56237deabefa86384f20b0dd418e4b5934c76
grdino <67813644+grdino@users.noreply.github.com>
2026-01-17 15:51:58 -0600

    Update index.js
    New

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/listings",
+      permanent: false,
+    },
+  };
+}
 
+export default function Home() {
+  return null;
+}
